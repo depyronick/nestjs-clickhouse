18c79873366e0b4e91eeb2fbe424a5bc1fda1822
Ali Demirci <alidemirci@gmail.com>
2022-03-28 17:21:44 +0300

    feat(registerAsync): initial implementation of registerAsync [NEEDS TEST]. ref #7

--- lib/clickhouse.module.ts
+++ lib/clickhouse.module.ts
@@ -1,11 +1,27 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, ModuleMetadata, Provider, Type } from '@nestjs/common';
 
 import {
   ClickHouseClient,
   ClickHouseClientOptions as ClickHouseNodeClientOptions
 } from '@depyronick/clickhouse-client';
 
+export const CLICKHOUSE_MODULE_OPTIONS = "CLICKHOUSE_MODULE_OPTIONS";
+
 export class ClickHouseModuleOptions extends ClickHouseNodeClientOptions { }
+
+export interface ClickHouseModuleOptionsFactory {
+  createClickhouseOptions(): Promise<ClickHouseModuleOptions> | ClickHouseModuleOptions;
+}
+
+export interface ClickHouseModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+  useExisting?: Type<ClickHouseModuleOptionsFactory>;
+  useClass?: Type<ClickHouseModuleOptionsFactory>;
+  useFactory?: (
+    ...args: any[]
+  ) => Promise<ClickHouseModuleOptions> | ClickHouseModuleOptions;
+  inject?: any[];
+  extraProviders?: Provider[];
+}
 
 @Module({})
 export class ClickHouseModule {
@@ -28,5 +44,40 @@ export class ClickHouseModule {
       providers: clients,
       exports: clients,
     };
+  }
+
+  static registerAsync(options: ClickHouseModuleAsyncOptions): DynamicModule {
+    return {
+      module: ClickHouseModule,
+      imports: options.imports,
+      providers: [
+        ...this.createAsyncProviders(options),
+        ...(options.extraProviders || [])
+      ]
+    }
+  }
+
+  private static createAsyncProviders(options: ClickHouseModuleAsyncOptions): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+
+    return [
+      this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass
+      }
+    ]
+  }
+
+  private static createAsyncOptionsProvider(options: ClickHouseModuleAsyncOptions): Provider {
+    if (options.useFactory) {
+      return {
+        provide: CLICKHOUSE_MODULE_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || []
+      }
+    }
   }
 }
